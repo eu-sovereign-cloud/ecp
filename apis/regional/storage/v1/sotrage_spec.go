18e2bf070e1b93bd8fc41c89e0c85a26d560dc85
cristi <cristian.mihai.guran@ionos.com>
2025-09-29 17:40:58 +0300

    fix: generate common models

---
+++ apis/regional/storage/v1/sotrage_spec.go
@@ -0,0 +1,18 @@
+package v1
+
+import (
+	"github.com/eu-sovereign-cloud/ecp/apis/regional/common"
+)
+
+type StorageSpec struct {
+	// Size of the storage in gigabytes
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=10000
+	SizeGB int64 `json:"sizeGB"`
+	// SKU of the storage
+	Sku string `json:"sku,omitempty"`
+
+	common.RegionalCommonData `json:"regionalCommonData,omitempty"`
+
+	common.RegionalCommonSpec `json:",inline"`
+}
