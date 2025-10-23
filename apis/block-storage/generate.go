4752c44910bb8e6a97159269e86d136e36414080
cguran-ionos <cristian-mihai.guran@ionos.com>
2025-10-23 11:32:17 +0300

    Generate: storage CRD for SKU (#17)

---
+++ apis/block-storage/generate.go
@@ -0,0 +1,6 @@
+//go:build crdgen
+
+package blockstorage
+
+//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../../.github/boilerplate.go.txt paths=./skus/v1/...
+//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./skus/v1/... output:crd:artifacts:config=../generated/crds/block-storage
