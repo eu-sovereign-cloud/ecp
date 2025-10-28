68fc61d2c95d807de7459abdf17e3af613f89e8a
cristi <cristian.mihai.guran@ionos.com>
2025-10-28 12:13:28 +0200

    fix: plural for storages

--- apis/regional/block-storage/generate.go
+++ apis/regional/block-storage/generate.go
@@ -3,4 +3,4 @@
 package blockstorage
 
 //go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../../../.github/boilerplate.go.txt paths=./skus/v1/...
-//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./skus/v1/... output:crd:artifacts:config=../../generated/crds/block-storage
+//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./skus/v1/... output:crd:artifacts:config=../../generated/crds/block-storages
