98960b509b3fc7c210a3da6a9ab583d20d3817eb
cristi <cristian.mihai.guran@ionos.com>
2025-11-05 18:01:56 +0200

    feat: generate storage instance crd

---
+++ foundation/api/regional/block-storage/generate.go
@@ -0,0 +1,9 @@
+//go:build crdgen
+
+package blockstorage
+
+//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../../../../.github/boilerplate.go.txt paths=./skus/v1/...
+//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./skus/v1/... output:crd:artifacts:config=../../generated/crds/block-storage
+
+//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../../../../.github/boilerplate.go.txt paths=./storages/v1/...
+//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./storages/v1/... output:crd:artifacts:config=../../generated/crds/block-storage
