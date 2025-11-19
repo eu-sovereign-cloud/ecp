e57a2b5e5a6e020751409709d83ea51d0f77c8bd
cristi <cristian.mihai.guran@ionos.com>
2025-11-19 11:52:38 +0200

    refactor: add vendor files

---
+++ foundation/gateway/vendor/github.com/eu-sovereign-cloud/ecp/foundation/api/network/group_version.go
@@ -0,0 +1,22 @@
+package network
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+const (
+	// Group is the group name used in this package
+	Group = "network.v1.secapi.cloud"
+	// Version is the version of the API
+	Version = "v1"
+)
+
+var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: Group, Version: Version}
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
