4f7efb12f015306bde079752210a977f6102d1a7
cristi <cristian.mihai.guran@ionos.com>
2025-11-21 12:32:05 +0200

    fix: pr checks, fail tests

--- foundation/gateway/internal/controller/global/region/get_region.go
+++ foundation/gateway/internal/controller/global/region/get_region.go
@@ -13,8 +13,9 @@ type GetRegion struct {
 	Repo   port.ResourceQueryRepository[*model.RegionDomain]
 }
 
-// NotDo retrieves a specific region, maps it to the domain, and then projects it to the SDK model.
+// Do retrieves a specific region, maps it to the domain, and then projects it to the SDK model.
 func (c *GetRegion) Do(ctx context.Context, nr port.NamespacedResource) (*model.RegionDomain, error) {
+	failTest
 	regionDomain := &model.RegionDomain{
 		Metadata: model.Metadata{Name: nr.GetName()},
 	}
