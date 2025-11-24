9c2af36dfe28dcc276f3a0d5d638cf05eed278e9
cristi <cristian.mihai.guran@ionos.com>
2025-11-24 11:12:20 +0200

    fix: generating crds

--- foundation/api/block-storage/generate.go
+++ foundation/api/block-storage/generate.go
@@ -2,5 +2,5 @@
 
 package blockstorage
 
-//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../../../.github/boilerplate.go.txt paths=./skus/v1/...
-//go:generate go run -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./skus/v1/... output:crd:artifacts:config=../generated/crds/block-storage
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../../../.github/boilerplate.go.txt paths=./skus/v1/...
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./skus/v1/... output:crd:artifacts:config=../generated/crds/block-storage
