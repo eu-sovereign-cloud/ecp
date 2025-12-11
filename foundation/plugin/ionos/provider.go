cd068de3ce6baed5ccc5272dac3bc5b7ce9aa590
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2025-12-11 16:26:45 +0100

    feat(delegator): remove old plugin stuff

--- foundation/plugin/ionos/provider.go
+++ foundation/plugin/ionos/provider.go
@@ -2,8 +2,6 @@ package ionos
 
 import (
 	"context"
-
-	"github.com/eu-sovereign-cloud/ecp/foundation/delegator/plugin"
 )
 
 // Provider implements the delegator ResourcePlugin interface for IONOS.
@@ -14,8 +12,4 @@ func (p *Provider) Name() string { return "ionoscloud" }
 func (p *Provider) Init(ctx context.Context) error {
 	// TODO: initialization logic
 	return nil
-}
-
-func init() {
-	plugin.Register(&Provider{})
 }
