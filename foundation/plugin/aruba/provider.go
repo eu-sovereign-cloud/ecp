b6c875aedc97b21a3dc13b78fb708530a8279105
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2025-12-11 16:26:45 +0100

    feat(delegator): remove old plugin stuff

--- foundation/plugin/aruba/provider.go
+++ foundation/plugin/aruba/provider.go
@@ -2,8 +2,6 @@ package aruba
 
 import (
 	"context"
-
-	"github.com/eu-sovereign-cloud/ecp/foundation/delegator/plugin"
 )
 
 // Provider implements the delegator ResourcePlugin interface for Aruba.
@@ -14,8 +12,4 @@ func (p *Provider) Name() string { return "aruba" }
 func (p *Provider) Init(ctx context.Context) error {
 	// TODO: initialization logic
 	return nil
-}
-
-func init() {
-	plugin.Register(&Provider{})
 }
