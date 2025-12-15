9aaab53d3c512c21ee0bef8b0537ee3b37d0e42f
cguran-ionos <cristian-mihai.guran@ionos.com>
2025-12-15 11:30:12 +0200

    feat: generate storage crd (#83)

--- foundation/api/regional/block-storage/generate.go
+++ foundation/api/regional/block-storage/generate.go
@@ -4,3 +4,6 @@ package blockstorage
 
 //go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../../../../.github/boilerplate.go.txt paths=./skus/v1/...
 //go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./skus/v1/... output:crd:artifacts:config=../../generated/crds/block-storage
+
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../../../../.github/boilerplate.go.txt paths=./instances/v1/...
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=./instances/v1/... output:crd:artifacts:config=../../generated/crds/block-storage
---
+++ foundation/api/regional/block-storage/instances/v1/storage_types.go
@@ -0,0 +1,35 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	genv1 "github.com/eu-sovereign-cloud/ecp/foundation/api/generated/types"
+	storage "github.com/eu-sovereign-cloud/ecp/foundation/api/regional/block-storage"
+)
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=storages,scope=Namespaced,shortName=storage
+// +k8s:openapi-gen=true
+
+// Storage is the API for getting storage SKU information
+type Storage struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   genv1.BlockStorageSpec   `json:"spec,omitempty"`
+	Status genv1.BlockStorageStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+type StorageList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Storage `json:"items"`
+}
+
+func init() {
+	storage.SchemeBuilder.Register(&Storage{}, &StorageList{})
+}
