31234f84890f6b2d6b69c5580b96c23858b54aa6
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2025-12-22 09:27:29 +0100

    feat(plugin): aruba e2e block storage plugin

--- foundation/plugin/aruba/provider.go
+++ foundation/plugin/aruba/provider.go
@@ -1,15 +1,3 @@
 package aruba
 
-import (
-	"context"
-)
-
-// Provider implements the delegator ResourcePlugin interface for Aruba.
-type Provider struct{}
-
-func (p *Provider) Name() string { return "aruba" }
-
-func (p *Provider) Init(ctx context.Context) error {
-	// TODO: initialization logic
-	return nil
-}
+// Aruba E2E Block Storage Plugin
