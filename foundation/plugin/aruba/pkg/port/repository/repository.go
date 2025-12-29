ccbae7be44c9e12385c9caf9eb745b9e43f9e447
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2025-12-29 16:39:27 +0100

    feat(plugin/aruba): generic repository port (issue #145)

---
+++ foundation/plugin/aruba/pkg/port/repository/repository.go
@@ -0,0 +1,111 @@
+package repository
+
+import (
+	"context"
+)
+
+// CLUDFunc is a generic function for CLUD (Create, Load, Update and Delete)
+// operations with instances of a given type.
+//
+// Important: type T must always be a reference (pointer) for the underlying
+// entity model to ensure consistent behavior across all repository operations.
+//
+// Important: because CLUD functions can mutate data on resources references
+// passed as parameters, please deepcopy them if you need to keep the original
+// data.
+type CLUDFunc[T any] func(ctx context.Context, resource T) error
+
+// Repository is a generic interface for a repository of resources.
+// It combines the Reader, Writer, and Watcher interfaces.
+//
+// Important: type T must always be a reference (pointer) for the underlying
+// entity model to ensure consistent behavior across all repository operations.
+//
+// Important: because CLUD functions can mutate data on resources references
+// passed as parameters, please deepcopy them if you need to keep the original
+// data.
+type Repository[T any] interface {
+	Reader[T]
+	Writer[T]
+	Watcher[T]
+}
+
+// Reader is a generic interface for reading resources from a repository.
+//
+// Important: type T must always be a reference (pointer) for the underlying
+// entity model to ensure consistent behavior across all repository operations.
+//
+// Important: because CLUD functions can mutate data on resources references
+// passed as parameters, please deepcopy them if you need to keep the original
+// data.
+type Reader[T any] interface {
+	// Load retrieves a single resource from the repository.
+	//
+	// The resource to be loaded is identified by the fields set in the
+	// provided 'resource' argument.
+	Load(ctx context.Context, resource T) error
+	// List retrieves a list of resources from the repository.
+	//
+	// The resources to be listed can be filtered by the fields set in the
+	// provided 'resource' argument.
+	List(ctx context.Context, resource T) ([]T, error)
+}
+
+// Writer is a generic interface for writing resources to a repository.
+//
+// Important: type T must always be a reference (pointer) for the underlying
+// entity model to ensure consistent behavior across all repository operations.
+//
+// Important: because CLUD functions can mutate data on resources references
+// passed as parameters, please deepcopy them if you need to keep the original
+// data.
+type Writer[T any] interface {
+	// Create adds a new resource to the repository.
+	Create(ctx context.Context, resource T) error
+	// Update modifies an existing resource in the repository.
+	Update(ctx context.Context, resource T) error
+	// Delete removes a resource from the repository.
+	Delete(ctx context.Context, resource T) error
+}
+
+// Watcher is a generic interface for watching for changes to resources in a
+// repository.
+type Watcher[T any] interface {
+	// Watch monitors a resource for changes.
+	// It returns a channel that will receive the updated resource whenever a
+	// change is detected.
+	//
+	// The 'resource' argument specifies which resource to watch, filtering by
+	// the fields that are set.
+	//
+	// The method also returns a 'cancel' function that must be called to stop
+	// watching for changes and clean up resources.
+	//
+	// Finally, it can return an error if setting up the watch fails.
+	//
+	// Example usage:
+	//   out, cancel, err := watcher.Watch(ctx, myResource)
+	//   if err != nil {
+	//     // handle error
+	//   }
+	//   defer cancel()
+	//
+	//   updateCount := 0
+	//   for {
+	//     select {
+	//     case updatedResource := <-out:
+	//       // Process updated resource
+	//       updateCount++
+	//       // Example condition: stop after 5 updates
+	//       if updateCount >= 5 {
+	//         // It will result on calling the deferred cancel to stop the
+	//         // watch
+	//         return
+	//       }
+	//     case <-ctx.Done():
+	//       // context was cancelled
+	//       return
+	//     }
+	//   }
+	Watch(ctx context.Context, resource T) (out chan T, cancel func(), err error)
+}
