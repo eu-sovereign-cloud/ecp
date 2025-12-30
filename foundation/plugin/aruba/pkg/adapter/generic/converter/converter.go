6c6b4d4d391416ce1231b6049d75df2fe0fba860
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2025-12-30 13:58:56 +0100

    feat(plugin/aruba): complete the generic converter type

--- foundation/plugin/aruba/pkg/adapter/generic/converter/converter.go
+++ foundation/plugin/aruba/pkg/adapter/generic/converter/converter.go
@@ -7,9 +7,43 @@ import (
 // GenericConverter is a generic implementation of the Converter interface.
 //
 // It uses two functions to perform the conversions.
-type GenericConverter[S any, A any] struct {
+type GenericConverter[S, A any] struct {
 	fromSECAToAruba aruba_converter_port.ConvertSECAToArubaFunc[S, A]
 	fromArubaToSECA aruba_converter_port.ConvertArubaToSECAFunc[S, A]
+}
+
+var _ aruba_converter_port.Converter[float64, int] = (*GenericConverter[float64, int])(nil)
+
+// NewGenericConverter creates and returns a new GenericConverter with its
+// internal conversion functions initialized to nil. These can be set later
+// using SetFromSECAToArubaFunc and SetFromArubaToSECAFunc.
+func NewGenericConverter[S, A any]() *GenericConverter[S, A] {
+	return &GenericConverter[S, A]{}
+}
+
+// NewGenericConverterWithFuncs creates and returns a new GenericConverter
+// initialized with the provided SECA to Aruba and Aruba to SECA conversion
+// functions.
+func NewGenericConverterWithFuncs[S, A any](
+	fromSECAToAruba aruba_converter_port.ConvertSECAToArubaFunc[S, A],
+	fromArubaToSECA aruba_converter_port.ConvertArubaToSECAFunc[S, A],
+) *GenericConverter[S, A] {
+	return &GenericConverter[S, A]{
+		fromSECAToAruba: fromSECAToAruba,
+		fromArubaToSECA: fromArubaToSECA,
+	}
+}
+
+// SetFromSECAToArubaFunc sets the conversion function responsible for
+// converting a SECA resource of type S to an Aruba resource of type A.
+func (c *GenericConverter[S, A]) SetFromSECAToArubaFunc(converterFunc aruba_converter_port.ConvertSECAToArubaFunc[S, A]) {
+	c.fromSECAToAruba = converterFunc
+}
+
+// SetFromArubaToSECAFunc sets the conversion function responsible for
+// converting an Aruba resource of type A to a SECA resource of type S.
+func (c *GenericConverter[S, A]) SetFromArubaToSECAFunc(converterFunc aruba_converter_port.ConvertArubaToSECAFunc[S, A]) {
+	c.fromArubaToSECA = converterFunc
 }
 
 // FromSECAToAruba converts a SECA resource to an Aruba resource using the
@@ -23,5 +57,3 @@ func (c *GenericConverter[S, A]) FromSECAToAruba(from S) (A, error) {
 func (c *GenericConverter[S, A]) FromArubaToSECA(from A) (S, error) {
 	return c.fromArubaToSECA(from)
 }
-
-var _ aruba_converter_port.Converter[float64, int] = (*GenericConverter[float64, int])(nil)
