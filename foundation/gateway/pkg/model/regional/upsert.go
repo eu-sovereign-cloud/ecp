cd2dff6922d774f46868c19cb7996c7a4a704394
Gabriela Limberea <gabriela.limberea@ionos.com>
2025-12-30 15:59:39 +0200

    add comments to UpsertParams and Scope models

--- foundation/gateway/pkg/model/regional/upsert.go
+++ foundation/gateway/pkg/model/regional/upsert.go
@@ -2,11 +2,12 @@ package regional
 
 import "github.com/eu-sovereign-cloud/ecp/foundation/gateway/pkg/model/scope"
 
+// UpsertParams - parameters for creating or updating resources. Implements port.IdentifiableResource.
 type UpsertParams struct {
 	scope.Scope
 
 	Name              string
-	IfUnmodifiedSince *int
+	IfUnmodifiedSince int
 }
 
 func (u *UpsertParams) GetName() string { return u.Name }
--- foundation/gateway/pkg/model/scope/scope.go
+++ foundation/gateway/pkg/model/scope/scope.go
@@ -1,5 +1,6 @@
 package scope
 
+// Scope - defines the scope of a resource with tenant and workspace. Implements port.Scope.
 type Scope struct {
 	Tenant    string
 	Workspace string
