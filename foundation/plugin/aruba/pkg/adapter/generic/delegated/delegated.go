52c50f6834a39012842d5d03cf483be6b782bd13
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2026-01-05 12:25:27 +0100

    feat(plugin/aruba): generic delegator logic - seca dependency resolution phase

--- foundation/plugin/aruba/pkg/adapter/generic/delegated/delegated.go
+++ foundation/plugin/aruba/pkg/adapter/generic/delegated/delegated.go
@@ -8,16 +8,27 @@ import (
 
 	converter_port "github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/port/converter"
 	delegated_port "github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/port/delegated"
+	resolver_port "github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/port/resolver"
 )
 
-type GenericDelegated[S seca_gateway_port.IdentifiableResource, A any] struct {
-	converter converter_port.Converter[S, A]
+type GenericDelegated[
+	S seca_gateway_port.IdentifiableResource, // SECA resource type
+	SB any, // SECA bundle type
+	AB any, // Aruba bundle type
+] struct {
+	secaResolver resolver_port.DependenciesResolver[S, SB]
+	converter    converter_port.Converter[SB, AB]
 }
 
-var _ delegated_port.Delegated[seca_gateway_port.IdentifiableResource] = (*GenericDelegated[seca_gateway_port.IdentifiableResource, any])(nil)
+var _ delegated_port.Delegated[seca_gateway_port.IdentifiableResource] = (*GenericDelegated[seca_gateway_port.IdentifiableResource, any, any])(nil)
 
-func (d *GenericDelegated[S, A]) Do(ctx context.Context, resource S) error {
-	_, err := d.converter.FromSECAToAruba(resource)
+func (d *GenericDelegated[S, SB, AB]) Do(ctx context.Context, resource S) error {
+	secaBundle, err := d.secaResolver.ResolveDependencies(ctx, resource)
+	if err != nil {
+		return err
+	}
+
+	_, err = d.converter.FromSECAToAruba(secaBundle)
 	if err != nil {
 		return err
 	}
