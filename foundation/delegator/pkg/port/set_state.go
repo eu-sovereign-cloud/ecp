30f6225f84a18d97e4da9f4bb8b35cabc13e1741
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2026-01-09 17:33:44 +0100

    fix: issues pointed in code review
      - only updade is sufficient (remove setStateFunc type and usages)
      - handle `pending` state
      - handle a single retry for creating and increase size operations

--- foundation/delegator/pkg/port/set_state.go
+++
@@ -1,9 +0,0 @@
-package port
-
-import (
-	"context"
-
-	gateway_port "github.com/eu-sovereign-cloud/ecp/foundation/gateway/pkg/port"
-)
-
-type SetStateFunc[T gateway_port.IdentifiableResource] func(ctx context.Context, resource T) error
