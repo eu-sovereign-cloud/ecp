13a748f0b9e4af9ed64a0f6e8478b634ee632606
Gabriela Limberea <115462667+glimberea@users.noreply.github.com>
2026-01-15 14:15:08 +0200

    feat: workspace gateway (#103)
    * workspace wip
    
    * add mapping functions for StatusDomain, StatusConditionDomain and ResourceStateDomain
    
    * add API to Domain and Domain to CR conversion funcs
    
    * add API to UpsertParams conversion func
    
    * add workspace controllers and handlers, delete not included yet
    
    * lint
    
    * add delete controller and handler
    
    * set resourceVersion in mapping functions and add workspace controller tests
    
    * move mapping functions around
    
    * lint
    
    * modify interfaceToString function to use jsonMarshalling, add stringToInterface func to preserve original data types

---
+++ foundation/gateway/internal/controller/regional/workspace/delete_workspace.go
@@ -0,0 +1,22 @@
+package workspace
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/eu-sovereign-cloud/ecp/foundation/gateway/pkg/model/regional"
+	"github.com/eu-sovereign-cloud/ecp/foundation/gateway/pkg/port"
+)
+
+type DeleteWorkspace struct {
+	Logger *slog.Logger
+	Repo   port.WriterRepo[*regional.WorkspaceDomain]
+}
+
+func (c *DeleteWorkspace) Do(ctx context.Context, ir port.IdentifiableResource) error {
+	domain := &regional.WorkspaceDomain{}
+	domain.Name = ir.GetName()
+	domain.Tenant = ir.GetTenant()
+
+	return c.Repo.Delete(ctx, domain)
+}
