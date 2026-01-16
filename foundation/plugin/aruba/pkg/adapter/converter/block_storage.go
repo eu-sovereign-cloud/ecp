fe89c717f6e8257e133250127d81d0024964c501
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2026-01-16 10:37:26 +0100

    refactor(block storage): propagate changes from main branch

--- foundation/plugin/aruba/pkg/adapter/converter/block_storage.go
+++ foundation/plugin/aruba/pkg/adapter/converter/block_storage.go
@@ -2,9 +2,10 @@ package converter
 
 import (
 	"github.com/Arubacloud/arubacloud-resource-operator/api/v1alpha1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"github.com/eu-sovereign-cloud/ecp/foundation/gateway/pkg/model"
 	"github.com/eu-sovereign-cloud/ecp/foundation/gateway/pkg/model/regional"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type BlockStorageConverter struct {
@@ -23,13 +24,12 @@ func (c *BlockStorageConverter) FromSECAToAruba(from *regional.BlockStorageDomai
 			Location: v1alpha1.Location{
 				Value: from.Spec.SourceImageRef.Region,
 			},
-			ProjectReference: v1alpha1.ResourceReference {
+			ProjectReference: v1alpha1.ResourceReference{
 				Name: from.Spec.SourceImageRef.Workspace,
 			},
-				
-			DataCenter: "IT-BG1",
-			BillingPeriod: "Monthly",
 
+			DataCenter:    "IT-BG1",
+			BillingPeriod: "Monthly",
 		},
 	}, nil
 
@@ -37,20 +37,18 @@ func (c *BlockStorageConverter) FromSECAToAruba(from *regional.BlockStorageDomai
 
 func (c *BlockStorageConverter) FromArubaToSECA(from *v1alpha1.BlockStorage) (*regional.BlockStorageDomain, error) {
 	return &regional.BlockStorageDomain{
-		Metadata: model.Metadata{
+		Metadata: regional.Metadata{
 			CommonMetadata: model.CommonMetadata{
-			Name: from.ObjectMeta.Name,
+				Name: from.ObjectMeta.Name,
 			},
 		},
 		Spec: regional.BlockStorageSpec{
 			SizeGB: int(from.Spec.SizeGb),
-			SkuRef: regional.ReferenceObject{
-				
-			},
+			SkuRef: regional.ReferenceObject{},
 			SourceImageRef: &regional.ReferenceObject{
-				Tenant: from.Spec.Tenant,
-				Region: from.Spec.Location.Value,
-				Workspace: from.Spec.ProjectReference.Name ,
+				Tenant:    from.Spec.Tenant,
+				Region:    from.Spec.Location.Value,
+				Workspace: from.Spec.ProjectReference.Name,
 			},
 		},
 	}, nil
