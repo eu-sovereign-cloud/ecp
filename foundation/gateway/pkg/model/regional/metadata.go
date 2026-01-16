c075fa5e0a9ca7c3e282e081f41021fc06ae5809
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2026-01-16 11:21:15 +0100

    fix(gateway tests): remove provisory elements

--- foundation/gateway/pkg/model/regional/metadata.go
+++ foundation/gateway/pkg/model/regional/metadata.go
@@ -13,11 +13,4 @@ type Metadata struct {
 	Annotations map[string]string
 	Extensions  map[string]string
 	Region      string
-	Tenant      string
-	Workspace   string
 }
-
-func (r *Metadata) GetTenant() string             { return r.Tenant }
-func (r *Metadata) GetWorkspace() string          { return r.Workspace }
-func (r *Metadata) SetTenant(tenant string)       { r.Tenant = tenant }
-func (r *Metadata) SetWorkspace(workspace string) { r.Workspace = workspace }
