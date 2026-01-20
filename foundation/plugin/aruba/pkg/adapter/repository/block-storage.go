f971875c34cb491a76c0dfecbf564fdd3880c41a
tommaso.floris <tommaso.floris@staff.aruba.it>
2026-01-20 12:42:37 +0100

    feat: Use context with cancel inside handle method

--- foundation/plugin/aruba/pkg/adapter/repository/block-storage.go
+++ foundation/plugin/aruba/pkg/adapter/repository/block-storage.go
@@ -12,6 +12,11 @@ import (
 
 type BlockStorageRepository = *repository.GenericRepository[*v1alpha1.BlockStorage, *v1alpha1.BlockStorageList]
 
+<<<<<<< HEAD
 func NewBlockStorageRepository(ctx context.Context, cli client.Client, ca crcache.Cache) BlockStorageRepository {
 	return repository.NewGenericRepository[*v1alpha1.BlockStorage, *v1alpha1.BlockStorageList](ctx, cli, ca)
+=======
+func NewStorageRepository(client client.Client, cache crcache.Cache) StorageRepository {
+	return generic_repository.NewGenericRepository[*v1alpha1.BlockStorage, *v1alpha1.BlockStorageList](client, cache)
+>>>>>>> e462b85 (feat: Use context with cancel inside handle method)
 }
