59711d8ad7e23e3354b4a236a35748f8d48c5e70
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2026-01-21 10:32:58 +0100

    chore(delegator): devx for dummy plugin delegator

---
+++ foundation/plugin/dummy/scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+IMG=${IMG:-"ecp-dummy-delegator"}
+VERSION=${VERSION:-"latest"}
+
+if [ -n "$REGISTRY" ]; then
+    IMAGE_NAME="${REGISTRY}/${IMG}:${VERSION}"
+else
+    IMAGE_NAME="${IMG}:${VERSION}"
+fi
+
+
+DOCKER_BUILD_CONTEXT="${SCRIPT_DIR}/../../../.."
+DOCKERFILE_PATH="${SCRIPT_DIR}/../build/Dockerfile"
+
+docker build -t "${IMAGE_NAME}" -f "${DOCKERFILE_PATH}" "${DOCKER_BUILD_CONTEXT}"
+
+echo "Image built: ${IMAGE_NAME}"
