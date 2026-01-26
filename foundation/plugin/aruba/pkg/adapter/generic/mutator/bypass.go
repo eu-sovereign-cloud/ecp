be99725b9139bba6fb60b388a0844b902d43f1ef
tommaso.floris <tommaso.floris@staff.aruba.it>
2026-01-26 18:24:22 +0100

    chore: fix import

--- foundation/plugin/aruba/pkg/adapter/generic/mutator/bypass.go
+++ foundation/plugin/aruba/pkg/adapter/generic/mutator/bypass.go
@@ -1,7 +1,7 @@
 package mutator
 
 import (
-	mutator_port "github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/port/mutator"
+	"github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/port/mutator"
 )
 
 // BypassMutateFunc is a helper function that bypasses mutation and returns nil.
@@ -10,14 +10,14 @@ func BypassMutateFunc[Mutable, Params any](mutable Mutable, params Params) error
 	return nil
 }
 
-var _ mutator_port.MutateFunc[any, any] = BypassMutateFunc[any, any]
+var _ mutator.MutateFunc[any, any] = BypassMutateFunc[any, any]
 
 // BypassMutator is a mutator that bypasses mutation and does nothing.
 // This is useful for scenarios where no mutation is needed on the target object.
 type BypassMutator[Mutable, Params any] struct{}
 
 // Ensure BypassMutator implements the Mutator interface
-var _ mutator_port.Mutator[any, any] = (*BypassMutator[any, any])(nil)
+var _ mutator.Mutator[any, any] = (*BypassMutator[any, any])(nil)
 
 // Mutate does nothing and returns nil.
 func (_ *BypassMutator[Mutable, Params]) Mutate(mutable Mutable, params Params) error {
