37f08ed80b3dbfec2cc1c1805812ab7511906eeb
tommaso.floris <tommaso.floris@staff.aruba.it>
2026-02-03 17:51:51 +0100

    chore: upgrade converter and handler func

--- foundation/plugin/aruba/pkg/adapter/handler/block-storage.go
+++ foundation/plugin/aruba/pkg/adapter/handler/block-storage.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"time"
 
 	"github.com/Arubacloud/arubacloud-resource-operator/api/v1alpha1"
 
@@ -9,9 +10,12 @@ import (
 	"github.com/eu-sovereign-cloud/ecp/foundation/gateway/pkg/model/regional"
 
 	"github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/adapter/generic/delegated"
+	resolver_bypass "github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/adapter/generic/resolver"
+
 	mutator_bypass "github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/adapter/generic/mutator"
 	"github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/port/converter"
 	repository "github.com/eu-sovereign-cloud/ecp/foundation/plugin/aruba/pkg/port/repository"
+	"k8s.io/apimachinery/pkg/api/errors"
 )
 
 // Ensure BlockStorageHandler implements the BlockStorage interface
@@ -21,6 +25,8 @@ var _ plugin.BlockStorage = (*BlockStorageHandler)(nil)
 // It is responsible for translating BlockStorageDomain resources to Aruba BlockStorage
 // and managing their lifecycle (Create/Delete).
 type BlockStorageHandler struct {
+	repository            repository.Repository[*v1alpha1.BlockStorage, *v1alpha1.BlockStorageList]
+	converter             converter.Converter[*regional.BlockStorageDomain, *v1alpha1.BlockStorage]
 	createDelegated       *delegated.GenericDelegated[*regional.BlockStorageDomain, *regional.BlockStorageDomain, *v1alpha1.BlockStorage]
 	deleteDelegated       *delegated.GenericDelegated[*regional.BlockStorageDomain, *regional.BlockStorageDomain, *v1alpha1.BlockStorage]
 	increaseSizeDelegated *delegated.GenericDelegated[*regional.BlockStorageDomain, *regional.BlockStorageDomain, *v1alpha1.BlockStorage]
@@ -30,35 +36,40 @@ type BlockStorageHandler struct {
 // It sets up the necessary delegated operations for creating and deleting WorkspaceDomain resources.
 // The handler uses bypass mutators since no mutation is needed on the Aruba Project objects.
 func NewBlockStorageHandler(repo repository.Repository[*v1alpha1.BlockStorage, *v1alpha1.BlockStorageList], conv converter.Converter[*regional.BlockStorageDomain, *v1alpha1.BlockStorage]) *BlockStorageHandler {
-	return &BlockStorageHandler{
-		createDelegated: delegated.NewStraightDelegated(
-			conv.FromSECAToAruba,
-			mutator_bypass.BypassMutateFunc[*v1alpha1.BlockStorage, *regional.BlockStorageDomain],
-			repo.Create,
-			func(p *v1alpha1.BlockStorage) bool {
-				return p.Status.Phase == v1alpha1.ResourcePhaseCreated
-			},
-			repo.WaitUntil,
-		),
-		deleteDelegated: delegated.NewStraightDelegated(
-			conv.FromSECAToAruba,
-			mutator_bypass.BypassMutateFunc[*v1alpha1.BlockStorage, *regional.BlockStorageDomain],
-			repo.Delete,
-			func(p *v1alpha1.BlockStorage) bool {
-				return p.Status.Phase == v1alpha1.ResourcePhaseDeleted
-			},
-			repo.WaitUntil,
-		),
-		increaseSizeDelegated: delegated.NewStraightDelegated(
-			conv.FromSECAToAruba,
-			mutator_bypass.BypassMutateFunc[*v1alpha1.BlockStorage, *regional.BlockStorageDomain],
-			repo.Update,
-			func(p *v1alpha1.BlockStorage) bool {
-				return p.Status.Phase == v1alpha1.ResourcePhaseCreated
-			},
-			repo.WaitUntil,
-		),
+	bsHandler := &BlockStorageHandler{
+		repository: repo,
+		converter:  conv,
 	}
+
+	bsHandler.createDelegated = delegated.NewStraightDelegated(
+		conv.FromSECAToAruba,
+		mutator_bypass.BypassMutateFunc[*v1alpha1.BlockStorage, *regional.BlockStorageDomain],
+		repo.Create,
+		func(p *v1alpha1.BlockStorage) bool {
+			return p.Status.Phase == v1alpha1.ResourcePhaseCreated
+		},
+		repo.WaitUntil,
+	)
+
+	bsHandler.deleteDelegated = delegated.NewStraightDelegated(
+		conv.FromSECAToAruba,
+		mutator_bypass.BypassMutateFunc[*v1alpha1.BlockStorage, *regional.BlockStorageDomain],
+		repo.Delete,
+		bsHandler.checkBsDeleteCondition,
+		repo.WaitUntil,
+	)
+
+	bsHandler.increaseSizeDelegated = delegated.NewDelegated(
+		resolver_bypass.BypassResolveDependenciesFunc[*regional.BlockStorageDomain],
+		conv.FromSECAToAruba,
+		bsHandler.ResolveBlockStorageDependencies,
+		BlockStorageMutateSizeFunc,
+		repo.Update,
+		bsHandler.checkBsIncreaseSizeCondition,
+		repo.WaitUntil,
+	)
+
+	return bsHandler
 }
 
 // Create creates a new BlockStorageDomain by creating an Aruba BlockStorage.
@@ -74,4 +85,46 @@ func (h *BlockStorageHandler) Delete(ctx context.Context, resource *regional.Blo
 // IncreaseSize increases the size of an existing BlockStorageDomain by updating the corresponding Aruba BlockStorage.
 func (h *BlockStorageHandler) IncreaseSize(ctx context.Context, resource *regional.BlockStorageDomain) error {
 	return h.increaseSizeDelegated.Do(ctx, resource)
+}
+
+func (h *BlockStorageHandler) checkBsDeleteCondition(resource *v1alpha1.BlockStorage) bool {
+	//TODO: refactor design completely
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	err := h.repository.Load(ctx, resource)
+
+	return errors.IsNotFound(err)
+}
+
+func (h *BlockStorageHandler) checkBsIncreaseSizeCondition(resource *v1alpha1.BlockStorage) bool {
+	//TODO: refactor design completely
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	size := resource.Spec.SizeGb
+
+	err := h.repository.Load(ctx, resource)
+
+	if err != nil {
+		return false
+	}
+
+	return resource.Spec.SizeGb == size && resource.Status.Phase == v1alpha1.ResourcePhaseCreated
+}
+
+func BlockStorageMutateSizeFunc(
+	mutable *v1alpha1.BlockStorage,
+	params *regional.BlockStorageDomain,
+) error {
+	mutable.Spec.SizeGb = int32(params.Spec.SizeGB)
+
+	return nil
+}
+
+func (h *BlockStorageHandler) ResolveBlockStorageDependencies(ctx context.Context, main *v1alpha1.BlockStorage) (*v1alpha1.BlockStorage, error) {
+	err := h.repository.Load(ctx, main)
+
+	return main, err
+
 }
