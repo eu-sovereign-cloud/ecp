b0a76675002ccb1f8007d11d49febd76d541c14a
tommaso.floris <tommaso.floris@staff.aruba.it>
2026-02-18 17:16:52 +0100

    chore: simulate delay inside dummy plugin

--- foundation/plugin/dummy/pkg/plugin/block_storage.go
+++ foundation/plugin/dummy/pkg/plugin/block_storage.go
@@ -3,6 +3,8 @@ package plugin
 import (
 	"context"
 	"log/slog"
+	"math/rand/v2"
+	"time"
 
 	"github.com/eu-sovereign-cloud/ecp/foundation/gateway/pkg/model/regional"
 )
@@ -17,15 +19,35 @@ func NewBlockStorage(logger *slog.Logger) *BlockStorage {
 
 func (b *BlockStorage) Create(ctx context.Context, resource *regional.BlockStorageDomain) error {
 	b.logger.Info("dummy block storage plugin: Create called", "resource_name", resource.GetName())
+	delay := blockStorageDelay()
+	b.logger.Info("dummy block storage plugin: Create finished", "resource_name", resource.GetName(), "delay(seconds)", delay)
 	return nil
 }
 
 func (b *BlockStorage) Delete(ctx context.Context, resource *regional.BlockStorageDomain) error {
 	b.logger.Info("dummy block storage plugin: Delete called", "resource_name", resource.GetName())
+	delay := blockStorageDelay()
+	b.logger.Info("dummy block storage plugin: Delete finished", "resource_name", resource.GetName(), "delay(seconds)", delay)
+
 	return nil
 }
 
 func (b *BlockStorage) IncreaseSize(ctx context.Context, resource *regional.BlockStorageDomain) error {
 	b.logger.Info("dummy block storage plugin: IncreaseSize called", "resource_name", resource.GetName(), "new_size_gb", resource.Spec.SizeGB)
+	delay := blockStorageDelay()
+	delay += blockStorageDelay()
+	b.logger.Info("dummy block storage plugin: IncreaseSize finished", "resource_name", resource.GetName(), "new_size_gb", resource.Spec.SizeGB, "delay(seconds)", delay)
 	return nil
+}
+
+func blockStorageDelay() int {
+	const base int = 30
+
+	var variation int = rand.IntN(60)
+
+	delay := base + variation
+
+	time.Sleep(time.Duration(delay) * time.Second)
+
+	return delay
 }
