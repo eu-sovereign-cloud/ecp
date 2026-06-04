1ccba32b0d9cbee66641b5dacce952cd1120520b
cristi <cristian.mihai.guran@ionos.com>
2026-06-04 16:43:43 +0300

    feat: delay should get context

--- foundation/plugin/dummy/pkg/plugin/network.go
+++ foundation/plugin/dummy/pkg/plugin/network.go
@@ -19,25 +19,34 @@ func NewNetwork(logger *slog.Logger) *Network {
 
 func (n *Network) Create(ctx context.Context, resource *regional.NetworkDomain) error {
 	n.logger.Info("dummy network plugin: Create called", "resource_name", resource.GetName())
-	delay := networkDelay()
+	delay, err := networkDelay(ctx)
+	if err != nil {
+		return err
+	}
 	n.logger.Info("dummy network plugin: Create finished", "resource_name", resource.GetName(), "delay(seconds)", delay)
 	return nil
 }
 
 func (n *Network) Delete(ctx context.Context, resource *regional.NetworkDomain) error {
 	n.logger.Info("dummy network plugin: Delete called", "resource_name", resource.GetName())
-	delay := networkDelay()
+	delay, err := networkDelay(ctx)
+	if err != nil {
+		return err
+	}
 	n.logger.Info("dummy network plugin: Delete finished", "resource_name", resource.GetName(), "delay(seconds)", delay)
 	return nil
 }
 
-func networkDelay() int {
+func networkDelay(ctx context.Context) (int, error) {
 	const base int = 30
 
 	variation := rand.IntN(60) //#nosec G404 -- math/rand/v2 is fine here: delay jitter is not security-sensitive
 
 	delay := base + variation
-	time.Sleep(time.Duration(delay) * time.Second)
-
-	return delay
+	select {
+	case <-time.After(time.Duration(delay) * time.Second):
+		return delay, nil
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	}
 }
--- foundation/plugin/dummy/pkg/plugin/workspace.go
+++ foundation/plugin/dummy/pkg/plugin/workspace.go
@@ -19,25 +19,34 @@ func NewWorkspace(logger *slog.Logger) *Workspace {
 
 func (w *Workspace) Create(ctx context.Context, resource *regional.WorkspaceDomain) error {
 	w.logger.Info("dummy workspace plugin: Create called", "resource_name", resource.GetName())
-	delay := workspaceDelay()
+	delay, err := workspaceDelay(ctx)
+	if err != nil {
+		return err
+	}
 	w.logger.Info("dummy workspace plugin: Create finished", "resource_name", resource.GetName(), "delay(seconds)", delay)
 	return nil
 }
 
 func (w *Workspace) Delete(ctx context.Context, resource *regional.WorkspaceDomain) error {
 	w.logger.Info("dummy workspace plugin: Delete called", "resource_name", resource.GetName())
-	delay := workspaceDelay()
+	delay, err := workspaceDelay(ctx)
+	if err != nil {
+		return err
+	}
 	w.logger.Info("dummy workspace plugin: Delete finished", "resource_name", resource.GetName(), "delay(seconds)", delay)
 	return nil
 }
 
-func workspaceDelay() int {
+func workspaceDelay(ctx context.Context) (int, error) {
 	const base int = 15
 
 	variation := rand.IntN(30) //#nosec G404 -- math/rand/v2 is fine here: delay jitter is not security-sensitive
 
 	delay := base + variation
-	time.Sleep(time.Duration(delay) * time.Second)
-
-	return delay
+	select {
+	case <-time.After(time.Duration(delay) * time.Second):
+		return delay, nil
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	}
 }
