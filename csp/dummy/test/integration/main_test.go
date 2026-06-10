14482cc3a9f67a635a336371ba7711bce0957a6d
tommaso.floris <tommaso.floris@staff.aruba.it>
2026-06-10 11:15:03 +0200

    chore: fix converter import inside e2e test

--- csp/dummy/test/integration/main_test.go
+++ csp/dummy/test/integration/main_test.go
@@ -29,6 +29,7 @@ import (
 	blockstoragev1 "github.com/eu-sovereign-cloud/ecp/foundation/models/kubernetes/api/regional/storage/block-storages/v1"
 	workspacev1 "github.com/eu-sovereign-cloud/ecp/foundation/models/kubernetes/api/regional/workspace/v1"
 	kubernetesadapter "github.com/eu-sovereign-cloud/ecp/foundation/persistence/adapters/kubernetes"
+	kubernetes2domain "github.com/eu-sovereign-cloud/ecp/foundation/persistence/adapters/kubernetes/domain"
 )
 
 const (
@@ -89,15 +90,15 @@ func TestMain(m *testing.M) {
 		dynamicClient,
 		blockstoragev1.BlockStorageGVR,
 		testLogger,
-		kubernetesadapter.MapBlockStorageDomainToCR,
-		kubernetesadapter.MapCRToBlockStorageDomain,
+		kubernetes2domain.MapBlockStorageDomainToCR,
+		kubernetes2domain.MapCRToBlockStorageDomain,
 	)
 	workspaceRepo = kubernetesadapter.NewRepoAdapter(
 		dynamicClient,
 		workspacev1.WorkspaceGVR,
 		testLogger,
-		kubernetesadapter.MapWorkspaceDomainToCR,
-		kubernetesadapter.MapCRToWorkspaceDomain,
+		kubernetes2domain.MapWorkspaceDomainToCR,
+		kubernetes2domain.MapCRToWorkspaceDomain,
 	)
 
 	// Wait for the test namespace to be created
