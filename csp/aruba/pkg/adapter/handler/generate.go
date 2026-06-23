9846e11f5d0a3438b3b9fd11f75c830c0f7fb458
FrancescoL96 <francesco29.fl@gmail.com>
2026-06-23 16:58:44 +0200

    fix: mockgen paths

--- csp/aruba/pkg/adapter/handler/generate.go
+++ csp/aruba/pkg/adapter/handler/generate.go
@@ -2,4 +2,4 @@ package handler
 
 //go:generate mockgen -package=handler -destination=./zz_mock_workspace_repository.go github.com/eu-sovereign-cloud/ecp/csp/aruba/pkg/port/repository Repository,Writer,Watcher,Reader
 //go:generate mockgen -package=handler -destination=./zz_mock_workspace_converter.go github.com/eu-sovereign-cloud/ecp/csp/aruba/pkg/port/converter Converter
-//go:generate mockgen -package=handler -destination=./zz_mock_reader_repo.go github.com/eu-sovereign-cloud/ecp/foundation/persistence/port ReaderRepo
+//go:generate mockgen -package=handler -destination=./zz_mock_reader_repo.go github.com/eu-sovereign-cloud/ecp/framework/kernel/port/persistence ReaderRepo
