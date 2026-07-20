c54d0577798e56b1dc835df900cd3063e1b3b222
FrancescoL96 <francesco29.fl@gmail.com>
2026-07-20 13:05:15 +0200

    test: remove role and role-assignment delegator and dummy integration tests

--- test/integration/delegator/main_test.go
+++ test/integration/delegator/main_test.go
@@ -24,10 +24,6 @@ import (
 	"github.com/eu-sovereign-cloud/ecp/framework/kernel"
 	"github.com/eu-sovereign-cloud/ecp/framework/kernel/port/persistence"
 	"github.com/eu-sovereign-cloud/ecp/framework/kernel/resource"
-	roledom "github.com/eu-sovereign-cloud/ecp/resource/authorization/v1/role"
-	radom "github.com/eu-sovereign-cloud/ecp/resource/authorization/v1/role-assignment"
-	rak8s "github.com/eu-sovereign-cloud/ecp/resource/authorization/v1/role-assignment/backend/kubernetes"
-	rolek8s "github.com/eu-sovereign-cloud/ecp/resource/authorization/v1/role/backend/kubernetes"
 	commondomain "github.com/eu-sovereign-cloud/ecp/resource/common/domain"
 	bsdom "github.com/eu-sovereign-cloud/ecp/resource/storage/v1/block-storage"
 	bsk8s "github.com/eu-sovereign-cloud/ecp/resource/storage/v1/block-storage/backend/kubernetes"
@@ -57,8 +53,6 @@ var (
 	workspaceRepo      persistence.Repo[*wsdom.Workspace]
 	blockStorageRepo   persistence.Repo[*bsdom.BlockStorage]
 	imageRepo          persistence.Repo[*imgdom.Image]
-	roleRepo           persistence.Repo[*roledom.Role]
-	roleAssignmentRepo persistence.Repo[*radom.RoleAssignment]
 	k8sClient          client.Client
 )
 
@@ -66,8 +60,6 @@ func TestMain(m *testing.M) {
 	// Initialize k8s scheme for client-go
 	s := runtime.NewScheme()
 	utilruntime.Must(scheme.AddToScheme(s))
-	utilruntime.Must(rolek8s.AddToScheme(s))
-	utilruntime.Must(rak8s.AddToScheme(s))
 	utilruntime.Must(wsk8s.AddToScheme(s))
 	utilruntime.Must(bsk8s.AddToScheme(s))
 	utilruntime.Must(imgk8s.AddToScheme(s))
@@ -115,22 +107,6 @@ func TestMain(m *testing.M) {
 		testLogger,
 		imgk8s.ImageToCR,
 		imgk8s.ImageFromCR,
-	)
-
-	roleRepo = k8sadapter.NewRepoAdapter(
-		dynamicClient,
-		rolek8s.RoleGVR,
-		testLogger,
-		rolek8s.RoleToCR,
-		rolek8s.RoleFromCR,
-	)
-
-	roleAssignmentRepo = k8sadapter.NewRepoAdapter(
-		dynamicClient,
-		rak8s.RoleAssignmentGVR,
-		testLogger,
-		rak8s.RoleAssignmentToCR,
-		rak8s.RoleAssignmentFromCR,
 	)
 
 	workspaceRepo = k8sadapter.NewNamespaceManagingRepoAdapter(
